0e2189449f43787e5ecf2fc097f64de2eef55c31
Lauren N Massey <laurennmassey@Laurens-MacBook-Pro.local>
2022-05-15 15:28:05 -0700

    fetchData in useEffect, able to see update to state

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,32 +1,48 @@
 import "./App.css";
 import Axios from "axios";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import Nav from "../../components/Nav/Nav";
 
 function App() {
-  const [info, setInfo] = useState([]);
+  const [info, setInfo] = useState({ photos: [] });
   const [requestError, setRequestError] = useState();
 
   const apiURL =
     "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=";
   const key = process.env.REACT_APP_API_KEY;
 
-  const fetchData = useCallback(async () => {
-    try {
-      const result = await Axios.get(`${apiURL}${key}`);
-      console.log(result.data);
-      setInfo(result.data);
-    } catch (err) {
-      setRequestError(err.message);
-    }
-  });
+  const fetchData = () => {
+    Axios.get(`${apiURL}${key}`)
+      .then((response) => {
+        const nasaInfo = response.data;
+
+        console.log(nasaInfo);
+        setInfo({ photos: nasaInfo.photos });
+        console.log("info", info);
+      })
+      .catch((error) => console.log(`Error: ${error}`));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  // const fetchData = useCallback(async () => {
+  //   try {
+  //     const result = await Axios.get(`${apiURL}${key}`);
+  //     console.log(result.data);
+  //     setInfo(result);
+  //     console.log(info);
+  //   } catch (err) {
+  //     setRequestError(err.message);
+  //   }
+  // });
 
   // const DEMO_KEY = process.env.DEMO_KEY;
 
   // const getNasa = () => {
   //   Axios.get(
-  //     "https://api.nasa.gov/planetary/apod?api_key=DFSit2HVwoqIrhoVuDnWR9WnLbUzteB2S9njk58v"
+  //     `https://api.nasa.gov/planetary/apod?api_key=${key}`
   //   ).then((response) => {
   //     console.log(response);
   //     setInfo(
@@ -37,7 +53,7 @@ function App() {
 
   // const getNasa = async () => {
   //   await fetch(
-  //     "https://api.nasa.gov/EPIC/api/natural/images?api_key=DFSit2HVwoqIrhoVuDnWR9WnLbUzteB2S9njk58v"
+  //     `https://api.nasa.gov/EPIC/api/natural/images?api_key=${key}`
   //   ).then((response) =>
   //     response.json().then((data) => {
   //       console.log(data);
